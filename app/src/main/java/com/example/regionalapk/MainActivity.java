ce02aa7ce4571519c742c04b9ca837aa917221d7
gabriel bavon <18mt469@esisalama.org>
2022-06-21 03:15:36 -0700

    changement d'activity selon la langue du telephone

---
+++ app/src/main/java/com/example/regionalapk/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.regionalapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saved) {
+        super.onCreate(saved);
+        setContentView(R.layout.activity_main1);
+
+    }
+
+}
